1199d77f938407b62656a4e04960c0a436a5018f
VictorJBozzelle <victorjbozzelle@gmail.com>
2019-09-03 15:32:04 -0500

    first commit

---
+++ Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Assignment2.Models;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult Venus()
+        {
+            return View();
+        }
+        public ViewResult Earth()
+        {
+            return View();
+        }
+        public ViewResult Mars()
+        {
+            return View();
+        }
+        public ViewResult Jupiter()
+        {
+            return View();
+        }
+        public ViewResult Uranus()
+        {
+            return View();
+        }
+    }
+}
